706d2fbefe72f563f6f49bb1b0940304a2d8a85e
Alexandre <ale.cco2002@gmail.com>
2023-09-08 20:01:34 -0300

    Começando o Banco de Dados

---
+++ app/src/main/java/intra/uninga/mobile/CriaBanco.java
@@ -0,0 +1,26 @@
+package intra.uninga.mobile;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by allanromanato on 5/27/15.
+ */
+public class CriaBanco extends SQLiteOpenHelper {
+    public CriaBanco(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
