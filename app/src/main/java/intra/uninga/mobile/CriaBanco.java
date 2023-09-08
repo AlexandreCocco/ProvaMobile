e624472efe5e66c7857d9ad34484e7e50ab8de46
Alexandre <ale.cco2002@gmail.com>
2023-09-08 20:06:03 -0300

    Criando um table de cadastro de livros

--- app/src/main/java/intra/uninga/mobile/CriaBanco.java
+++ app/src/main/java/intra/uninga/mobile/CriaBanco.java
@@ -13,6 +13,13 @@ public class CriaBanco extends SQLiteOpenHelper {
     public CriaBanco(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
+        private static final String NOME_BANCO = "banco.db";
+        private static final String TABELA = "livros";
+        private static final String ID = "_id";
+        private static final String TITULO = "titulo";
+        private static final String AUTOR = "autor";
+        private static final String EDITORA = "editora";
+        private static final int VERSAO = 1;
 
     @Override
     public void onCreate(SQLiteDatabase db) {
