6b97d4cfa32b1a83b9c55176f8a81cc3198a6bfd
Alexandre <ale.cco2002@gmail.com>
2023-09-08 21:09:44 -0300

    Códido para mostrar os dados no activity_list

--- app/src/main/java/intra/uninga/mobile/ListActivity.java
+++ app/src/main/java/intra/uninga/mobile/ListActivity.java
@@ -1,14 +1,29 @@
 package intra.uninga.mobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class ListActivity extends AppCompatActivity {
 
+public class ListActivity extends Activity {
+    private ListView lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        BancoController crud = new BancoController(getBaseContext());
+        Cursor cursor = crud.carregaDados();
+
+        String[] nomeCampos = new String[] {CriaBanco.ID, CriaBanco.TITULO};
+        int[] idViews = new int[] {R.id.idLivro, R.id.nomeLivro};
+
+        SimpleCursorAdapter adaptador = new SimpleCursorAdapter(getBaseContext(),
+                R.layout.activity_list,cursor,nomeCampos,idViews, 0);
+        lista = (ListView)findViewById(R.id.listView);
+        lista.setAdapter(adaptador);
     }
 }
